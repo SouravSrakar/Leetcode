6160ac7098c54786b145a1144a14ac3eafd73e9c
Sourav Sarkar <98875674+SouravSrakar@users.noreply.github.com>
2026-07-10 12:55:05 +0530

    Time: 1 ms (99.86%) | Memory: 46.7 MB (64.82%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans=new ArrayList<>();
+        for(int i=1;i<=n;i++)
+        {
+            boolean div3=(i%3==0);
+            boolean div5=(i%5==0);
+            if(div3 && div5)
+            {
+                ans.add("FizzBuzz");
+            }
+            else if(div3)
+            {
+                ans.add("Fizz");
+            }
+            else if(div5)
+            {
+                ans.add("Buzz");
+            }
+            else
+            {
+                ans.add(String.valueOf(i));
+            }
+        }
+        return ans;
+    }
+}
